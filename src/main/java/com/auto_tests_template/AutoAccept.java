cd9b1117d0931be5501673753eea5de6c61ac8b0
ZeO <102325825+YiamZeO@users.noreply.github.com>
2024-01-21 21:28:15 +0300

    Изменения для подтверждения через чат с помощью команды

--- src/main/java/com/auto_tests_template/AutoAccept.java
+++ src/main/java/com/auto_tests_template/AutoAccept.java
@@ -14,7 +14,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Set;
+
 import org.apache.commons.io.FileUtils;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -27,8 +30,8 @@ public class AutoAccept {
         ChromeOptions chromeOptions = new ChromeOptions()
                 // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
                 .addArguments("--remote-allow-origins=*")
-                .addArguments("--window-size=1920,1080")
-                .addArguments("--headless");
+                .addArguments("--window-size=1920,1080");
+//                .addArguments("--headless");
         WebDriver driver = new ChromeDriver(chromeOptions);
         WebDriverRunner.setWebDriver(driver);
     }
@@ -46,48 +49,41 @@ public class AutoAccept {
             } catch (InterruptedException | IOException e) {
                 throw new RuntimeException(e);
             }
-            SelenideElement ditBotChat = $x("//a[.//h3[@role='button' and text()='DITauthBot']]");
+            SelenideElement ditBotChat = $x("//a[.//h3[text()='DITauthBot']]");
             ditBotChat.shouldBe(Condition.visible, Duration.ofSeconds(60L));
             ditBotChat.click();
-            SelenideElement acceptButton = $$x("//div[contains(@id, 'message')]" +
-                    "//button[./span[text()='Да это я']]").last();
-            acceptButton.shouldBe(Condition.visible, Duration.ofSeconds(60L));
             SelenideElement lastMessage = $$x("//div[contains(@id, 'message')]" +
-                    "//div[@class='content-inner']//div[@class='text-content clearfix with-meta']").last();
-            SelenideElement lastMessageWithButton = $$x("//div[contains(@id, 'message') and " +
-                    ".//button[./span[text()='Да это я']]]//div[@class='content-inner']" +
-                    "//div[@class='text-content clearfix with-meta']" ).last();
-            SelenideElement scrollButton =
-                    $x("//button[./i[@class='AafG9_xBi_2eJ_bFNnNg icon icon-arrow-down']]");
+                    "//div[@class='content-inner']/div").last();
+            executeJavaScript("window.open(arguments[0], '_blank')", "https://web.telegram.org/a/");
+            Set<String> windowHandles = WebDriverRunner.getWebDriver().getWindowHandles();
+            Iterator<String> windowsIterator = windowHandles.iterator();
+            String firstWindow = windowsIterator.next();
+            String secondWindow = windowsIterator.next();
+            Selenide.switchTo().window(secondWindow);
+            SelenideElement controlChat = $x("//a[.//h3[text()='Saved Messages']]");
+            controlChat.shouldBe(Condition.visible, Duration.ofSeconds(60L));
+            controlChat.click();
+//            SelenideElement eircChat = $x("//a[.//h3[text()='ЕИРЦ']]");
+//            eircChat.shouldBe(Condition.visible, Duration.ofSeconds(60L));
+//            eircChat.click();
+//            SelenideElement vpnChat = $x("//a[.//h3[text()='VPN DIT запросы']]");
+//            eircChat.shouldBe(Condition.visible, Duration.ofSeconds(60L));
+//            vpnChat.click();
             System.out.println("---> Job started [" + LocalDateTime.now() + "]");
-            int lifeCounter = 0;
-            int acceptCounter = 0;
             while (!hasShutdownTrigger()){
-                if (scrollButton.isDisplayed()){
-                    scrollButton.click();
-                    lifeCounter = 0;
+                if (lastMessage.getOwnText().contains("Vpn bot!")) {
+                    Selenide.switchTo().window(firstWindow);
+                    SelenideElement acceptButton = $x("//div[contains(@id, 'message')]" +
+                    "//button[./span[text()='Да это я']]");
+                    acceptButton.click();
+                    Selenide.switchTo().window(secondWindow);
+                    SelenideElement inputField = $x("//div[@id='message-input-text']/div/div/div");
+                    inputField.setValue("Accepted");
+                    SelenideElement sendButton = $x("//button[@title='Send Message']");
+                    sendButton.click();
+                    System.out.println("---> Accepted [" + LocalDateTime.now() + "]");
                 }
-                if (lastMessage.getOwnText().equals(lastMessageWithButton.getOwnText())){
-                    if(acceptButton.isDisplayed()) {
-                        acceptButton.click();
-                        System.out.println("---> Accepted [" + LocalDateTime.now() + "]");
-                        lifeCounter = 0;
-                        acceptCounter += 1;
-                    }
-                }
-                else{
-                    acceptCounter = 0;
-                }
-                if (!lastMessage.isDisplayed() || !acceptButton.isDisplayed() || acceptCounter >= 10){
-                    Selenide.refresh();
-                    System.out.println("---> Refreshed [" + LocalDateTime.now() + "]");
-                    acceptCounter = 0;
-                    if ((!lastMessage.isDisplayed() || !acceptButton.isDisplayed()) && (!scrollButton.isDisplayed())){
-                        lifeCounter += 1;
-                        if (lifeCounter >= 10)
-                            System.out.println("---> ERROR browser sleeping [" + LocalDateTime.now() + "]");
-                    }
-                }
+
                 try {
                     Thread.sleep(Duration.ofSeconds(2L));
                 } catch (InterruptedException e) {
